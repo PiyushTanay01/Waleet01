9c12d857116dd841c45179f6419e9788aa4f17a3
PiyushTanay01 <piyushtanay434@gmail.com>
2024-09-01 00:47:22 +0530

    Deployment1

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -18,20 +18,41 @@ export async function getBalance() {
 }
 
 
-export default async function() {
+// export default async function() {
+//     const balance = await getBalance();
+
+//     return <div className="w-screen">
+//         <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+//             Transfer
+//         </div>
+//         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+//             <div>
+//                 <AddMoney />
+//             </div>
+//             <div>
+//                 <BalanceCard amount={balance.amount} locked={balance.locked} />
+//             </div>
+//         </div>
+//     </div>
+// }
+const TransferPage = async () => {
     const balance = await getBalance();
 
-    return <div className="w-screen">
-        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
-            Transfer
-        </div>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
-            <div>
-                <AddMoney />
+    return (
+        <div className="w-screen">
+            <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+                Transfer
             </div>
-            <div>
-                <BalanceCard amount={balance.amount} locked={balance.locked} />
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+                <div>
+                    <AddMoney />
+                </div>
+                <div>
+                    <BalanceCard amount={balance.amount} locked={balance.locked} />
+                </div>
             </div>
         </div>
-    </div>
-}
+    );
+};
+
+export default TransferPage;
